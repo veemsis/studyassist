74186b8e2521daf45667e8d7017ccd513f25de2c
veemsis <c.cisowski@gmail.com>
2014-02-21 17:24:39 +0100

    Timer class, new Watch Object not working

--- Script.js
+++ Script.js
@@ -1,17 +1,34 @@
-function Timer(minutes){
-    for(var min = minutes;min>=0;min --){
-        $('#minutes').delay(1000).replaceWith('<div id="minutes">' + min + '</div>');
-        
-        
-        for (var sec = 10;sec >= 0;sec-- ){
-            $('#seconds').delay(1000).fadeIn('slow').replaceWith('<div id="seconds">' + sec + '</div>');
-            $('#seconds').fadeOut('slow');  
+function Timer(){
+
+    this.setMinutes = function (minutes) {
+        this.minutes = minutes;
+    };
+    
+    this.timeOut = function () {
+        for (var min = minutes; min >= 0; min--) {
+            $('#minutes').replaceWith('<div id="minutes>' + min + '</div>');
+            $('#minutes').fadeIn('fast');
+            $('#minutes').delay(1000).fadeOut('fast');
+
+
+            for (var sec = 10; sec >= 0; sec--) {
+                $('#seconds').delay(1000).replaceWith('<div id="seconds">' + sec + '</div>');
+                $('#seconds').fadeIn('fast');
+                $('#seconds').delay(1000).fadeOut('fast');
+
+
+
+            }
         }
+
+
+
     }
 }
-
+var Watch = new Timer();
 $(document).ready(function () {
-    Timer(1);
+    Watch.setMinutes(2);
+    Watch.timeOut;
 
 });
 

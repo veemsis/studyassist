4bc9daa9e2e26823f885aa93678458494836740c
veemsis <c.cisowski@gmail.com>
2014-02-21 18:21:56 +0100

    setTimeout function added

--- Script.js
+++ Script.js
@@ -3,32 +3,33 @@ function Timer(){
     this.setMinutes = function (minutes) {
         this.minutes = minutes;
     };
-    
-    this.timeOut = function () {
+
+    this.start = function () {
         for (var min = minutes; min >= 0; min--) {
-            $('#minutes').replaceWith('<div id="minutes>' + min + '</div>');
-            $('#minutes').fadeIn('fast');
-            $('#minutes').delay(1000).fadeOut('fast');
+            setTimeout(function () {
+                $('#minutes').html(min).fadeIn().fadeOut();
+            }, 2000);
+            console.log(min);
 
 
             for (var sec = 10; sec >= 0; sec--) {
-                $('#seconds').delay(1000).replaceWith('<div id="seconds">' + sec + '</div>');
-                $('#seconds').fadeIn('fast');
-                $('#seconds').delay(1000).fadeOut('fast');
 
-
-
+                setTimeout(function () {
+                    $('#seconds').html(sec).fadeIn().fadeOut();
+                }, 2000);
+                console.log(sec);
             }
         }
 
 
 
-    }
+    };
 }
 var Watch = new Timer();
+
 $(document).ready(function () {
-    Watch.setMinutes(2);
-    Watch.timeOut;
+    Watch.setMinutes(20);
+    Watch.start();
 
 });
 

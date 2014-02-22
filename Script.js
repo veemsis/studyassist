ef99cc6824c72cf57bfaa7d99a4a3a9cab44c287
veemsis <c.cisowski@gmail.com>
2014-02-22 14:16:07 +0100

    1 step completed, timer is working

--- Script.js
+++ Script.js
@@ -4,37 +4,42 @@ function Timer(){
     var sec;
     var minuty;
     var pausedSec, pausedMin;
-    
-    
+
+
     this.start = function (minutes, seconds) {
         min = minutes;
         sec = seconds;
-        
-        
-            minuty = setInterval(function () {
-                if(sec === 0 && min ===0){
-                    clearInterval(minuty);
-                }
-                else if(sec === 0){
-                    min = min-1;
-                    console.log("Minute is gone " + min);
-                    $('#minutes').html(min).fadeIn();
-                    sec = 59;
-                    }else{
-                        console.log("second is gone " + sec);
-                        $('#seconds').html(sec).fadeIn().fadeOut();
-                        sec = sec-1;
-                    }
-                
-            }, 1000);     
-             };
+
+        $('#minutes').html(min).fadeIn();
+        $('#seconds').html(sec).fadeIn().fadeOut();
+        sec = sec - 1;
+
+
+        minuty = setInterval(function () {
+            if (sec === 0 && min === 0) {
+                clearInterval(minuty);
+            }
+            else if (sec === 0) {
+                min = min - 1;
+                console.log("Minute is gone " + min);
+                $('#minutes').html(min).fadeIn();
+                $('#seconds').html("59").fadeIn().fadeOut();
+                sec = 58;
+            } else {
+                console.log("second is gone " + sec);
+                $('#seconds').html(sec).fadeIn().fadeOut();
+                sec = sec - 1;
+            }
+
+        }, 1000);
+    };
 
              this.pause = function () {
                  if (isPaused == false) {
                      isPaused = true;
                      console.log('Pause - sec = ' + sec);
-                     var pausedSec = sec;
-                     var pausedMin = min;
+                     pausedSec = sec;
+                     pausedMin = min;
                      clearInterval(minuty);
                      console.log('pauseSec = ' + pausedSec);
 
@@ -58,7 +63,7 @@ var Watch = new Timer();
 $(document).ready(function () {
     $('#start').click(function () {
 
-        Watch.start(2,0);
+        Watch.start(2,5);
     });
     $('#pause').click(function () {
         Watch.pause();
